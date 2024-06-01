b4d09412a94ea74a66039c3ff4740186db4e5cd7
unknown <mezghanimohamedaymen@gmail.com>
2024-06-01 23:48:08 +0100

    [add] create cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import React from 'react'
+import Cards from './components/Cards';
 
 function App() {
   return (
-    <></>
+    <>
+      <Cards Title="Project"  />
+      <Cards Title="Services" icon="" />
+    </>
   );
 }
 
