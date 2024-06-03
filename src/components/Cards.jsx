0c40b2ca5c74903faf3110be4ab082385d1576a2
unknown <mezghanimohamedaymen@gmail.com>
2024-06-03 02:27:56 +0100

    [fix] update cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,15 +1,9 @@
 import React, { useEffect, useState } from 'react';
-// import gitIcon from '../assets/images/icons/github.png'
-// import eyeIcon from '../assets/images/icons/eye.png'
-// import detailsIcon from '../assets/images/icons/file-circle-info.png'
 import { fetchLogo } from "../services/api";
-
 import '../assets/styles/style.css';
 
 function Cards(props) {
-    const { title, type, icon, imgSrc, links } = props;
-
-    const isDisabled = true
+    const { title, type, imgSrc, links, skills } = props;
     const cardClass = type === 'Projects'
         ? 'flex flex-col text-white justify-center items-center md:py-4 md:px-4 bg_card rounded-3xl p-4 text-center'
         : 'flex flex-col text-white justify-center items-center md:py-4 md:px-4 bg_card rounded-3xl';
@@ -17,17 +11,33 @@ function Cards(props) {
 
     const [logos, setlogo] = useState([]);
     useEffect(() => {
-        const getLogo = async () => {
-            try {
-                const result = await fetchLogo(links);
-                setlogo(result);
-            } catch (error) {
-                console.error("Error fetching images : ", error);
+        if (type === "Projects") {
+            const getLogo = async () => {
+                try {
+                    const result = await fetchLogo(links, type);
+                    setlogo(result);
+                } catch (error) {
+                    console.error("Error fetching images : ", error);
+                }
             }
+            getLogo();
         }
-        getLogo();
-    }, [links]);
+    }, [links, type]);
+    useEffect(() => {
+        if (type === "Services") {
+            const getLogo = async () => {
+                try {
+                    const result = await fetchLogo(skills, type);
+                    setlogo(result);
+                } catch (error) {
+                    console.error("Error fetching images : ", error);
+                }
+            }
+            getLogo();
+        }
+    }, [skills, type]);
 
+    const array = type === "Projects" ? Object.keys(links) : skills
     return (
         <div className={cardClass}>
             {type === 'Projects' && (
@@ -35,22 +45,20 @@ function Cards(props) {
                     <img src={imgSrc} alt="" />
                 </div>
             )}
-            <h3 className='text-2xl mb-3 text-center mt-5'><span>{icon ? icon : null}</span> {title}</h3>
+            <h3 className='text-2xl mb-3 text-center mt-5 mx-2'>{title}</h3>
             <ul className={`my-5 ${type === 'Projects' ? 'flex justify-evenly mt-5 w-full' : 'grid grid-cols-5 md:gap-6 md:grid-cols-8 gap-3'}`}>
-
-                {Object.keys(links).map((key, index) => {
-                    const linkValue = links[key];
+                {array.map((key, index) => {
+                    const linkValue = type === "Projects" ? links[key] : null;
                     return (
                         <li key={index}>
                             <a className='shadow' href={linkValue !== "null" ? linkValue : '/'}>
-                                {/* Use dynamic icons fetched from fetchIcons */}
                                 <img src={logos[key]} alt={key} className="w-10 h-10" />
                             </a>
                         </li>
                     );
                 })}
             </ul>
-        </div>
+        </div >
     )
 }
 
