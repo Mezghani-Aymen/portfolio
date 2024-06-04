39cb04f4c62551ef5ca08b8a5896217129d9d4fd
unknown <mezghanimohamedaymen@gmail.com>
2024-06-04 12:52:26 +0100

    [fix] update cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,13 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { fetchLogo } from "../services/api";
 import '../assets/styles/style.css';
+import NavItems from './NavItems';
+import Overlay from './Overlay';
+import { click } from '@testing-library/user-event/dist/click';
+
 
 function Cards(props) {
     const { title, type, imgSrc, links, skills } = props;
+
     const cardClass = type === 'Projects'
-        ? 'flex flex-col text-white justify-center items-center md:py-4 md:px-4 bg_card rounded-3xl p-4 text-center'
+        ? 'flex flex-col text-white justify-center items-center md:py-4 md:px-4 bg_card rounded-3xl p-4 text-center relative '
         : 'flex flex-col text-white justify-center items-center md:py-4 md:px-4 bg_card rounded-3xl';
 
+    const [overlayVisible, setOverlayVisible] = useState(false);
+    const [overlayContent, setOverlayContent] = useState(null);
+
+    const handleNavItemClick = (key) => {
+        const details = links[key].details || [];
+        console.log(details);
+        setOverlayContent({ key, details });
+        setOverlayVisible(true);
+
+    };
+
+    const closeOverlay = () => {
+        setOverlayVisible(false);
+        setOverlayContent(null);
+    };
 
     const [logos, setlogo] = useState([]);
     useEffect(() => {
@@ -37,7 +57,7 @@ function Cards(props) {
         }
     }, [skills, type]);
 
-    const array = type === "Projects" ? Object.keys(links) : skills
+    const arrayIcons = type === "Projects" ? Object.keys(links) : skills
     return (
         <div className={cardClass}>
             {type === 'Projects' && (
@@ -47,17 +67,25 @@ function Cards(props) {
             )}
             <h3 className='text-2xl mb-3 text-center mt-5 mx-2'>{title}</h3>
             <ul className={`my-5 ${type === 'Projects' ? 'flex justify-evenly mt-5 w-full' : 'grid grid-cols-5 md:gap-6 md:grid-cols-8 gap-3'}`}>
-                {array.map((key, index) => {
-                    const linkValue = type === "Projects" ? links[key] : null;
+                {arrayIcons.map((key, index) => {
+                    console.log(key);
                     return (
-                        <li key={index}>
-                            <a className='shadow' href={linkValue !== "null" ? linkValue : '/'}>
-                                <img src={logos[key]} alt={key} className="w-10 h-10" />
-                            </a>
-                        </li>
+                        <NavItems
+                            key={index}
+                            type={type === "Projects" ? "link" : "null"}
+                            href={type === "Projects" && index === 2 ? "null" : type === "Projects" ? links[key] : "null"}
+                            action={type === "Projects" && index === 2 ? () => handleNavItemClick(key) : null}
+                        >
+                            <img src={logos[key]} alt={key} className="w-10 h-10" />
+                        </NavItems>
+
                     );
                 })}
             </ul>
+
+            {overlayVisible && (
+                <Overlay closeOverlay={closeOverlay} content={overlayContent} />
+            )}
         </div >
     )
 }
