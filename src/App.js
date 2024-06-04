6a1cf49e11a1ba134c93b181727ba5f8ac693783
unknown <mezghanimohamedaymen@gmail.com>
2024-06-04 12:57:45 +0100

    [fix] update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react'
-import Cards from './components/Cards';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { getNavigation } from './utils/navigation';
+import Header from "./components/Header";
+
 
 function App() {
+  const navigation = getNavigation();
   return (
-    <>
-      <Cards Title="Project"  />
-      <Cards Title="Services" icon="" />
-    </>
+    <Router>
+      <Header links={navigation} />
+      < Routes >
+        {
+          navigation.map((item, index) => (
+            <Route
+              key={index}
+              path={item.path}
+              element={<item.layout />}
+            />
+          ))
+        }
+      </Routes>
+
+    </Router >
   );
 }
+
 
 export default App;
