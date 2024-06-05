da12052e69bbdf95058de8d9e2840901b7bc90de
unknown <mezghanimohamedaymen@gmail.com>
2024-06-05 15:36:41 +0100

    [fix] update Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,27 @@
-import React, { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import NavItems from './NavItems';
+import hamburger from '../assets/images/icons/burger.png'
 
 function Header(props) {
     const location = useLocation();
-    const [active, setActive] = useState(false);
+    const isActiveLink = (path) => location.pathname === path;
 
-    // Function to determine if a link is active
-    const isActiveLink = (path) => {
-        return location.pathname === path;
-    };
+    useEffect(() => {
+        if (location.pathname === "/" || location.pathname === "") {
+            document.documentElement.style.setProperty('--activeColor', "black");
+        } else {
+            document.documentElement.style.setProperty('--activeColor', "rgb(var(--blue))");
+        }
+    }, [location.pathname]);
 
     return (
-        <nav className='text-white p-5 flex justify-between absolute top-0 w-screen'>
-            <div className=''>
+        <nav className='flex text-white p-5 justify-between  w-screen'>
+            <div className='text-3xl flex justify-between w-auto'>
                 <p>Portfolio</p>
             </div>
-            <div className='w-60 text-white'>
-                <ul className='flex justify-between'>
+            <div className='w-96 text-white  hidden sm:block '>
+                <ul className={`flex justify-evenly  text-2xl  `}>
                     {props.links.map((route, index) => (
                         <NavItems key={index} type="link" href={route.path} active={isActiveLink(route.path)}>
                             {route.label}
