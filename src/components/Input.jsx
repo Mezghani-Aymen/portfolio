0849207c4d784643a110d1bfd4207fafe7f564ba
unknown <mezghanimohamedaymen@gmail.com>
2024-06-05 15:30:41 +0100

    [add] create Input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function Input(props) {
+    const { type, id, text } = props;
+
+    const commonStyles = {
+        backgroundColor: "transparent",
+        border: "1px solid rgb(0, 238, 255)",
+        borderRadius: "15px",
+        padding: "15px",
+    };
+
+    return (
+        type === "text" ? (
+            <input
+                type={type}
+                id={id}
+                placeholder={text}
+                className='w-fill h-9 my-2'
+                style={commonStyles}
+                onFocus={(e) => e.target.style.color = "rgb(0, 238, 255)"}
+                onBlur={(e) => e.target.style.color = "white"}
+            />
+        ) : (
+            <textarea
+                id={id}
+                placeholder={text}
+                className='w-fill h-28 my-2'
+                style={commonStyles}
+                onFocus={(e) => e.target.style.color = "rgb(0, 238, 255)"}
+                onBlur={(e) => e.target.style.color = "white"}
+            />
+        )
+    );
+}
+
+export default Input;
