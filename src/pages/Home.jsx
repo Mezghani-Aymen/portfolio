1b40ac758e5cc6ba0fa60aae688cd939342011a7
unknown <mezghanimohamedaymen@gmail.com>
2024-06-05 15:38:26 +0100

    [fix] update Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,82 @@
-import React from 'react'
-// import photo from "../assets/images/mezghani_mohamedAymen.png";
+import React, { useEffect, useState } from 'react';
+import photo from "../assets/images/mezghani_mohamedAymen.png";
 import "../assets/styles/style.css";
+import Button from '../components/Buttons'
+import { fetchLogo } from "../services/api";
+import { Link } from 'react-router-dom';
+import ii from "../assets/images/icons/github.png";
+import Buttons from '../components/Buttons';
+
 function Home() {
+    const [logos, setlogo] = useState([]);
+
+    useEffect(() => {
+        const getLogo = async () => {
+            try {
+                const result = await fetchLogo("Home",);
+                setlogo(result);
+            } catch (error) {
+                console.error("Error fetching images : ", error);
+            }
+        }
+        getLogo();
+
+    }, []);
+    const socialArray =
+        [
+            {
+                name: "facebook",
+                path: ""
+            },
+            {
+                name: "github",
+                path: ""
+            },
+            {
+                name: "linkdin",
+                path: ""
+            },
+        ]
+    const className = (false ? "nav-link active " : "nav-link" && true ? " shadow-blue" : "");
+
+    const href = ""
     return (
         <>
             <div className="shape"></div>
-            {/* <main className=' w-10/12 mx-auto flex items-center h-40 justify-between text-white'>
-                <div className="mx-10">
-                    <p>
-                        Hey
-                        names
-                        job
-                    </p>
-                    <p>sdqdq</p>
-                    <p>icons</p>
-                    <p>button</p>
-                </div>
-                <div className='rounded-3xl overflow-hidden img_container' style={{ height: 'auto', width: '390px' }}>
-                    <img alt='Img_Profile' src={photo} className=' w-fill h-fill  ' style={{}} />
-                </div>
-
-                {/* <div className='container bg-gray-600 text-white h-60 items-center'>
-                    qsdq
-                    <div className='flex justify-between'>
-                        <div className='bg-white text-black '>sqdsqda</div>
-                        <div className='bg-gray-600 '>sqdsqda</div>
+            <main className='justify-around w-screen flex items-center text-white sm:px-0 px-5 sm:py-5 pb-5 flex-wrap md:py-0 xl:mt-8 mt-3 '>
+                <div className="mx-10  text-2xl flex flex-col  ">
+                    <div className='mb-9 h-auto'>
+                        <p>Hey there , I'm</p>
+                        <p id='Span_Name ' className='text-4xl my-3'>Mezghani Mohamed Aymen</p>
+                        <p id='Span_Job' className='text-3xl c-bleu '>Software  Development</p>
                     </div>
-                </div>  
-            </main> */}
+                    <p id='discription'>Let's team up and make awesome stuff online. <br />
+                        Let's build something awesome together</p>
+                    <div className='mx-auto w-max my-5'>
+                        <ul className='my-5 flex justify-between mt-5 w-48'>
+                            <Link to={href} className={className}> <img src={ii} alt={""} className="w-10 h-10" /></Link>
+                            <Link to={href} className={className}> <img src={ii} alt={""} className="w-10 h-10" /></Link>
+                            <Link to={href} className={className}> <img src={ii} alt={""} className="w-10 h-10" /></Link>
+                            {/* {socialArray.map((item, index) => {
+                                return (
+                                    <NavItems
+                                        key={index}
+                                        type="link"
+                                        href={item.path}
+                                    >
+                                        <img src={item.name} alt={key} className="w-10 h-10" />
+                                    </NavItems>
+
+                                );
+                            })} */}
+                        </ul>
+                    </div>
+                    <Buttons text={<span>Download CV</span>} className="btn-home glassmorphism py-2 px-11  w-auto  mx-auto " />
+                </div>
+                <div className='rounded-3xl overflow-hidden img_container sm:my-6 mt-9' style={{ width: '390px' }}>
+                    <img alt='Img_Profile' src={photo} className=' w-fill h-fill' />
+                </div>
+            </main >
         </>
     )
 }
