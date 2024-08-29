e2ad16ab2ec2a26f3732980376b3e92a166f7bd1
unknown <mezghanimohamedaymen@gmail.com>
2024-08-29 13:59:35 +0100

    [fix] clear code as comments

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
+import './assets/css/tailwind.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { getNavigation } from './utils/navigation';
-import Header from "./components/Header";
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { getNavigation } from './utils/navigation';
+// import Header from "./components/Header";
 
 
 function App() {
-  const navigation = getNavigation();
+  // const navigation = getNavigation();
   return (
-    <Router>
-      <Header links={navigation} />
-      < Routes >
-        {
-          navigation.map((item, index) => (
-            <Route
-              key={index}
-              path={item.path}
-              element={<item.layout />}
-            />
-          ))
-        }
-      </Routes>
+    <></>
+    // <Router>
+    //   <Header links={navigation} />
+    //   < Routes >
+    //     {
+    //       navigation.map((item, index) => (
+    //         <Route
+    //           key={index}
+    //           path={item.path}
+    //           element={<item.layout />}
+    //         />
+    //       ))
+    //     }
+    //   </Routes>
 
-    </Router >
+    // </Router >
   );
 }
 
