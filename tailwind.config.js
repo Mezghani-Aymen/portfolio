e4de100808e42eaebd04593e533400de0abf85c3
unknown <mezghanimohamedaymen@gmail.com>
2024-08-29 13:54:51 +0100

    [add] tailwind init

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",  // Include all relevant file types
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
