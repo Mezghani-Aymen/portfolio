7248bd06ea9b9a1b1cffeab7da5564c2688e4848
unknown <mezghanimohamedaymen@gmail.com>
2024-08-31 22:35:53 +0100

    [fix] update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,57 +1,62 @@
 import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import NavItems from './NavItems';
-import hamburger from '../assets/images/icons/burger.png';
-import useActiveColor from '../hooks/useActiveColor';
-import useIsMobile from '../hooks/useIsMobile';
 
-function Header(props) {
-    const location = useLocation();
-    const isActiveLink = (path) => location.pathname === path;
+const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
 
-
-    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-    const mobile = useIsMobile()
-    useActiveColor(mobile);
-
-    const toggleMobileMenu = () => {
-        setIsMobileMenuOpen(!isMobileMenuOpen);
-    };
-
-    const closeMobileMenu = () => {
-        setIsMobileMenuOpen(false);
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
     };
 
     return (
-        <nav className='flex text-white p-5 justify-between w-screen'>
-            <div className='text-3xl flex justify-between w-auto'>
-                <p>Portfolio</p>
-            </div>
-            <div className='sm:hidden'>
-                <img src={hamburger} alt="Menu" className="w-10 h-10" onClick={toggleMobileMenu} />
-            </div>
-            {isMobileMenuOpen && (
-                <div className="fixed top-0 left-0 h-screen w-screen bg-black z-10 flex flex-col items-center justify-center">
-                    <ul className="flex flex-col items-center">
-                        {props.links.map((route, index) => (
-                            <NavItems key={index} type="link" href={route.path} active={isActiveLink(route.path)} action={closeMobileMenu}>
-                                {route.label}
-                            </NavItems>
-                        ))}
+        <header className="w-screen p-6">
+            <div className=" flex justify-between items-center ">
+                <h1 className="text-white text-3xl underline-half">
+                    <span className=" text-[#D76B30]">P</span>
+                    <span >ortfolio</span></h1>
+                <nav className="hidden lg:flex space-x-4 text-xl">
+                    <ul className="flex space-x-4">
+                        <li>
+                            <a href="#projects" className="text-white hover:text-[#D76B30]">Projects</a>
+                        </li>
+                        <li>
+                            <a href="#services" className="text-white hover:text-[#D76B30]">Services</a>
+                        </li>
+                        <li>
+                            <a href="#contact" className="text-white hover:text-[#D76B30] me-5">Contact</a>
+                        </li>
                     </ul>
+                </nav>
+                <div className="lg:hidden ">
+                    <button onClick={toggleMenu} className="text-white focus:outline-none">
+                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+            {isOpen && (
+                <div className="fixed inset-0 bg-black flex flex-col items-center justify-center z-50">
+                    <ul className="flex flex-col items-center space-y-4">
+                        <li>
+                            <a href="" className="text-purple-500 hover:text-purple-600">Home</a>
+                        </li>
+                        <li>
+                            <a href="#projects" className="text-white hover:text-purple-600">Projects</a>
+                        </li>
+                        <li>
+                            <a href="#services" className="text-white hover:text-purple-600">Services</a>
+                        </li>
+                        <li>
+                            <a href="#contact" className="text-white hover:text-purple-600">Contact</a>
+                        </li>
+                    </ul>
+                    {/* <button onClick={toggleMenu} className="mt-4 text-white focus:outline-none">
+                        Close
+                    </button> */}
                 </div>
             )}
-            <div className='w-96 text-white hidden sm:block'>
-                <ul className='flex justify-evenly text-2xl'>
-                    {props.links.map((route, index) => (
-                        <NavItems key={index} type="link" href={route.path} active={isActiveLink(route.path)}>
-                            {route.label}
-                        </NavItems>
-                    ))}
-                </ul>
-            </div>
-        </nav>
+        </header>
     );
-}
+};
 
 export default Header;
