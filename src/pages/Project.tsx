02a6e7f32d1e301fd99221eedb5b1cceb2ae2e07
unknown <mezghanimohamedaymen@gmail.com>
2024-09-01 15:59:06 +0100

    [fix] update  Project content

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,45 +1,27 @@
-// Projects.tsx
 
 import React from 'react';
 import ProjectCard from '../components/Cards';
+//@ts-ignore
+import x from '../assets/images/mtx.png'
 
 const Projects = () => {
     const projectData = [
         {
-            title: 'TWINDER',
+            title: 'Microsoft Tech Club Sfax-ISIMS',
             description: 'A live geolocation app for finding tweets and twitter users around you.',
-            liveLink: 'https://example.com/twinder', // Update with actual link
-            githubLink: 'https://github.com/example/twinder',
-            image: '/path/to/twinder-image.png', // Update with actual image path
-        },
-        // {
-        //     title: 'LIVENTS',
-        //     description: 'A video streaming app with live geolocation for streaming events.',
-        //     liveLink: 'https://example.com/livents', // Update with actual link
-        //     githubLink: 'https://github.com/example/livents',
-        //     image: '/path/to/livents-image.png', // Update with actual image path
-        // },
-        // {
-        //     title: 'MOOVE',
-        //     description: 'Mobile app for booking instant pickup & dropoff across major cities.',
-        //     liveLink: 'https://example.com/moove', // Update with actual link
-        //     githubLink: 'https://github.com/example/moove',
-        //     image: '/path/to/moove-image.png', // Update with actual image path
-        // },
-        // {
-        //     title: 'MOOVE',
-        //     description: 'Mobile app for booking instant pickup & dropoff across major cities.',
-        //     liveLink: 'https://example.com/moove', // Update with actual link
-        //     githubLink: 'https://github.com/example/moove',
-        //     image: '/path/to/moove-image.png', // Update with actual image path
-        // },
+            liveLink: '', // Update with actual link
+            githubLink: 'https://github.com/Mezghani-Aymen/MTC-website-Project_Club',
+            image: 'mtx.png', // Update with actual image path
+        }
     ];
 
     return (
-        <section className="py-10 px-4 md:px-5 overflow-x-hidden ">
+
+        <section className="py-10 px-4 md:px-9 overflow-x-hidden ">
+
             <div className=" mx-auto text-left">
-                <h2 className="text-4xl font-bold mb-4">Featured Projects:</h2>
-                <p className="mb-8">I have worked on many projects over the course of being a Web Developer, here are a few of my live, real-world projects.</p>
+                <h2 className="text-4xl font-bold mb-4 text-[#D76B30]">Featured Projects</h2>
+                <p className="mb-8 text-[#E0E0E0]">I have worked on many projects over the course of being a Web Developer, here are a few of my live, real-world projects.</p>
                 <div className="pe-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 
                  gap-8">
                     {projectData.map((project, index) => (
@@ -49,7 +31,7 @@ const Projects = () => {
                             description={project.description}
                             liveLink={project.liveLink}
                             githubLink={project.githubLink}
-                            image={project.image}
+                            image={x}
                         />
                     ))}
                 </div>
