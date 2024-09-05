92a32bc302d38c941f1f6d6ff102d1e25b8e0284
unknown <mezghanimohamedaymen@gmail.com>
2024-09-05 23:14:36 +0100

    [fix] update project section

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -9,17 +9,16 @@ const Projects = () => {
         {
             title: 'Microsoft Tech Club Sfax-ISIMS',
             description: 'A live geolocation app for finding tweets and twitter users around you.',
-            liveLink: '', // Update with actual link
+            liveLink: '',
             githubLink: 'https://github.com/Mezghani-Aymen/MTC-website-Project_Club',
-            image: 'mtx.png', // Update with actual image path
+            image: 'mtx.png',
         }
     ];
 
     return (
 
-        <section className="pt-[6rem] px-4 md:px-9 overflow-x-hidden " id="projects" data-aos="fade-down">
-
-            <div className=" mx-auto text-left">
+        <section className="pt-[6rem] px-4 md:px-9 overflow-hidden " id="projects">
+            <div className=" mx-auto text-left" data-aos="fade-down">
                 <h2 className="text-4xl font-bold mb-4 text-[#D76B30]">Featured Projects</h2>
                 <p className="mb-8 text-[#E0E0E0]">I have worked on many projects over the course of being a Web Developer, here are a few of my live, real-world projects.</p>
                 <div className="pe-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 
