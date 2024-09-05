18ea4fdacdc7373498abcb3290ae47242af9f814
unknown <mezghanimohamedaymen@gmail.com>
2024-09-05 23:13:50 +0100

    [fix] update contact section

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,6 +1,13 @@
+import Input from 'components/Input';
 import React from 'react';
 
 const Contact: React.FC = () => {
+    const inputs = [
+        { type: "text", id: "name", placeholder: "Full Name", label: "Name" },
+        { type: "email", id: "email", placeholder: "Email@gmail.com", label: "Email" },
+        { type: "text", id: "subject", placeholder: "Subject Title", label: "Subject" },
+        { type: "textarea", id: "message", placeholder: "Message ...", label: "Message" }
+    ];
     return (
         <div className="md:flex w-full container mx-auto rounded-lg pt-[6rem] p-6 " id='contact'>
             <div className="flex-1 md:border-r md:border-b-0 border-b mb-5 pb-6 border-accentColor md:pr-5 md:mr-5" data-aos="fade-right">
@@ -22,28 +29,13 @@ const Contact: React.FC = () => {
             <div className="flex-1 " data-aos="fade-left">
                 <h2 className="text-lg font-semibold text-[#A3C4E0]">Contact me, let's make magic together</h2>
                 <form className="mt-4 flex flex-col">
-                    <div className="mb-4">
-                        <label htmlFor="name" className="block text-[#F5E5D5]">Name</label>
-                        <input type="text" id="name" placeholder="Your Name"
-                            className="w-full p-2 mt-1 bg-transparent border border-[#D76B30] text-[#F5E5D5] rounded focus:outline-none focus:ring-2 focus:ring-[#D76B30]" />
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="email" className="block text-[#F5E5D5]">Email</label>
-                        <input type="email" id="email" placeholder="Your Email"
-                            className="w-full p-2 mt-1 bg-transparent border border-[#D76B30] text-[#F5E5D5] rounded focus:outline-none focus:ring-2 focus:ring-[#D76B30]" />
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="subjet" className="block text-[#F5E5D5]">Subject</label>
-                        <input type="text" id="subjet" placeholder="Your Subjet"
-                            className="w-full p-2 mt-1 bg-transparent border border-[#D76B30] text-[#F5E5D5] rounded focus:outline-none focus:ring-2 focus:ring-[#D76B30]" />
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="message" className="block text-[#F5E5D5]">Message</label>
-                        <textarea id="message" rows={4} placeholder="Your Message"
-                            className="w-full p-2 mt-1 bg-transparent border border-[#D76B30] text-[#F5E5D5] rounded focus:outline-none focus:ring-2 focus:ring-[#D76B30]"></textarea>
-                    </div>
+                    {inputs.map((input, index) => (
+                        <div className="mb-4" key={index}>
+                            <Input type={input.type} id={input.id} placeholder={input.placeholder} label={input.label} />
+                        </div>
+                    ))}
                     <button type="submit"
-                        className="mx-auto  sm:w-1/4 sm:px-4  bg-[#D76B30] text-white rounded  w-2/4 px-0 py-2 hover:bg-[#A3C4E0]  ">Send</button>
+                        className="mx-auto  sm:w-1/4 sm:px-4  bg-[#D76B30] text-secondry hover:text-primary rounded  w-2/4 px-0 py-2 hover:bg-[#A3C4E0]  ">Send</button>
                 </form>
             </div>
         </div>
