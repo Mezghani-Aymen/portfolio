96af6c3805c41c971487f8bd2bff3922152a7328
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2025-06-22 22:56:46 +0100

    fix(Header): add missing props to NavbarMobile component and clean up formatting

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,20 +3,18 @@ import Navbar from './Navbar';
 import NavbarMobile from './NavbarMobile';
 import useNavigationMenu from 'hooks/useNavigationMenu';
 
-
 const Header = () => {
-    const { toggleMenu, isOpen } = useNavigationMenu();
+    const { toggleMenu, isOpen, scrollToTopAndCloseMenu, closeMenu } = useNavigationMenu();
 
     return (
         <header className="w-screen p-6 fixed top-0 z-20 bg-black">
-            <div className=" flex justify-between items-center ">
+            <div className="flex justify-between items-center">
                 <h1 className="text-white text-3xl underline-half">
-                    <span className=" text-[#D76B30]">P</span>
-                    <span >ortfolio</span>
+                    <span className="text-[#D76B30]">P</span>
+                    <span>ortfolio</span>
                 </h1>
                 <Navbar />
-
-                <div className="sm:hidden ">
+                <div className="sm:hidden">
                     <button onClick={toggleMenu} className="text-white focus:outline-none">
                         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
@@ -24,7 +22,9 @@ const Header = () => {
                     </button>
                 </div>
             </div>
-            {isOpen && (<NavbarMobile />)}
+            {isOpen && (
+                <NavbarMobile scrollToTopAndCloseMenu={scrollToTopAndCloseMenu} closeMenu={closeMenu} />
+            )}
         </header>
     );
 };
