1bda6cab8e0d65efaa0e7443bd2057618017a0bb
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2025-09-22 21:43:00 +0100

    feat(frontend-contexts): add getContextInstance helper

---
+++ frontend/src/utils/getContextInstance.ts
@@ -0,0 +1,22 @@
+import { useContext, Context } from "react";
+
+/**
+ * Safely get the value from a React context.
+ * Throws an error if the context is undefined (not provided).
+ *
+ * @param context - The React context to consume
+ * @param componentName - Optional component name for the error message
+ * @returns The context value
+ */
+
+function getContextInstance<T>(context: Context<T | undefined>, componentName?: string): T {
+    const value = useContext(context);
+    if (value === undefined) {
+        throw new Error(
+            `${componentName ? componentName : "Component"} must be used within its Provider`
+        );
+    }
+    return value;
+}
+
+export default getContextInstance;
