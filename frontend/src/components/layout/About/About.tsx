6d780eb413cb989d20867c3b9374aedbabbac342
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2025-09-27 11:18:33 +0100

    feat(frontend-components/layer): add About section

---
+++ frontend/src/components/layout/About/About.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Section from '../../ui/Section';
+import AboutContent from './AboutContent';
+
+const About = () => {
+
+    return (
+        <Section dataAos="fade-up" dataAosDelay="200" className="max-w-8xl mx-auto flex flex-col items-center px-6 justify-between pt-10 gap-7 " id="about">
+            <h1 className="text-[#D76B30] text-4xl font-bold w-full text-center " data-aos="fade-up">About me</h1>
+            <AboutContent />
+        </Section>
+    );
+};
+
+
+export default About;
