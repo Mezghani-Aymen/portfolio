f3696f7e642bef270303400cac2b127fe2d452d8
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2025-10-01 18:32:56 +0100

    style(frontend-components/layout):
    adjust height values of CallToAction component

--- frontend/src/components/layout/CallToAction/CallToAction.tsx
+++ frontend/src/components/layout/CallToAction/CallToAction.tsx
@@ -5,7 +5,7 @@ import Section from '../../ui/Section'
 
 export default function CallToAction() {
   return (
-    <Section className="relative flex sm:h-[300px] h-[350px] w-full items-center justify-center overflow-hidden bg-gray-950 text-center sm:px-0 px-5 ">
+    <Section className="relative flex sm:h-[400px] h-[450px] w-full items-center justify-center overflow-hidden bg-gray-950 text-center sm:px-0 px-5 ">
       <Ripple />
       <CallToActionContent />
     </Section>
