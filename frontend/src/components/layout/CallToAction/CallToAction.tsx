aea10cdcb9451b82b4a962d53f0a3fc5a521925c
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2025-10-01 18:32:14 +0100

    refactor(frontend-components/layout):
    replace div with semantic section tag of CallToAction component

--- frontend/src/components/layout/CallToAction/CallToAction.tsx
+++ frontend/src/components/layout/CallToAction/CallToAction.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Ripple } from '../../ui/Ripple'
 import CallToActionContent from './CallToActionContent'
+import Section from '../../ui/Section'
 
 export default function CallToAction() {
   return (
-    <div className="relative flex sm:h-[300px] h-[350px] w-full items-center justify-center overflow-hidden bg-gray-950 text-center sm:px-0 px-5 ">
+    <Section className="relative flex sm:h-[300px] h-[350px] w-full items-center justify-center overflow-hidden bg-gray-950 text-center sm:px-0 px-5 ">
       <Ripple />
       <CallToActionContent />
-    </div>
+    </Section>
   )
 }
