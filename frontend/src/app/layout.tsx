dd5194903f6f0478a806d001a1864599f5e77110
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2025-10-01 18:18:49 +0100

    refactor(frontend-app):
    move Header usage from file Page (Home) to file Layout (App)

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import Header from "../components/layout/Header/Header";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header />
         {children}
       </body>
     </html>
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,7 @@
-import Header from "@/components/layout/Header/Header";
 
 export default function Home() {
   return (
     <>
-      <Header />
     </>
   );
 }
