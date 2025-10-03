f2e599678cde2ca82f3d1ec8fe748d99b4acdaea
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2025-10-03 22:28:13 +0100

    feat(frontend-app):
    add Project section to App page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/layout/Hero/Hero";
 import About from "../components/layout/About/About";
+import Project from "../components/layout/Project/Project";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
+      <Project />
     </>
   );
 }
