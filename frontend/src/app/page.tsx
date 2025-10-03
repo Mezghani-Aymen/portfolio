3464c3456c642abe05be16683d493fae7dd16e17
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2025-10-03 22:27:57 +0100

    feat(frontend-app):
    add About section to App page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/layout/Hero/Hero";
+import About from "../components/layout/About/About";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <About />
     </>
   );
 }
