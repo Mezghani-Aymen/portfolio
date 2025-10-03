b8536053e731b642276ab63910726db68bc61034
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2025-10-03 22:48:50 +0100

    feat(frontend-app):
    add CallToAction section to App page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/layout/Hero/Hero";
 import About from "../components/layout/About/About";
 import Project from "../components/layout/Project/Project";
 import Skills from "../components/layout/Skills/Skills";
+import CallToAction from "../components/layout/CallToAction/CallToAction";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <About />
       <Project />
       <Skills />
+      <CallToAction />
     </>
   );
 }
