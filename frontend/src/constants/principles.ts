df1fcc3dcd5c0cdcee10c06bab965ef61d0bfdf0
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2025-10-07 14:25:30 +0100

    feat(frontend-contants): add principles

---
+++ frontend/src/constants/principles.ts
@@ -0,0 +1,19 @@
+import { PencilRuler, TrendingUp, Zap } from "lucide-react";
+
+export const principles = [
+    {
+        title: "Performance",
+        description: "Fast, Responsive, and Accessible sites.",
+        icon: Zap
+    },
+    {
+        title: "Pixel Precision",
+        description: "Turning designs into accurate, beautiful interfaces.",
+        icon: PencilRuler,
+    },
+    {
+        title: "Continuous Growth",
+        description: "Always exploring new tools and modern best practices.",
+        icon: TrendingUp,
+    },
+];
