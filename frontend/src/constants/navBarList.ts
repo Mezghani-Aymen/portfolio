bb5b64a86a0e51374a10c626f2f2ef62837a0d5c
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2025-10-07 14:24:54 +0100

    refactor(frontend-constants): update path of navbarList

--- frontend/src/constants/navBarList.ts
+++ frontend/src/constants/navBarList.ts
@@ -1,7 +1,7 @@
 export const navBarList = [
-    { path: "", smooth: true, duration: 500, label: 'Home' },
+    { path: "hero", smooth: true, duration: 500, label: 'Home' },
     { path: "about", smooth: true, duration: 500, label: 'About' },
     { path: "projects", smooth: true, duration: 500, label: 'Projects' },
     { path: "skills", smooth: true, duration: 500, label: 'Skills' },
-    { path: "contact", smooth: true, duration: 500, label: 'Connect', className: "me-5" }
+    { path: "/contact", smooth: true, duration: 500, label: 'Connect', className: "me-5" }
 ]
