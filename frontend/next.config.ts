0175b2f044df04b7f801cfd74a65162f234e64c5
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2025-11-17 00:55:42 +0100

    fix (frontend): update next config file

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    images: { unoptimized: true },
+  output: "export",
+  images: { unoptimized: true },
 
-    basePath: "/portfolio/",
-    assetPrefix: "/portfolio/"
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio/"
 };
 
 module.exports = nextConfig;
