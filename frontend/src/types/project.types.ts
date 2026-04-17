30f1ae953c4609f5f815477deb0e717272efe4ee
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2026-04-18 00:16:00 +0100

    feat(types): define ProjectType enum and refactor project interfaces

--- frontend/src/types/project.types.ts
+++ frontend/src/types/project.types.ts
@@ -1,9 +1,35 @@
-export interface IProject {
+export enum ProjectType {
+    Internal = "internship",
+    Personal = "personal"
+}
+
+interface BaseProject {
     title: string;
-    type: "internship" | "personal";
     description: string;
     techStack: string[];
     imageUrl: string;
-    liveUrl?: string;
-    githubUrl?: string;
+    deepDive?: {
+        challenges: string[];
+        solutions: string[];
+        results: string[];
+        architecture?: string;
+        gallery?: string[];
+    };
 }
+
+export interface PersonalProject extends BaseProject {
+    type: ProjectType.Personal;
+    liveUrl: string;
+    githubUrl: string;
+}
+
+export interface InternalProject extends BaseProject {
+    type: ProjectType.Internal;
+    company: ICompany
+}
+
+export interface ICompany {
+    name: string,
+    email: string
+}
+export type IProject = PersonalProject | InternalProject;
