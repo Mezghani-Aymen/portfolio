02ae00229dfc1b4a6e13dc5f0b73c822f0cff376
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2026-04-19 18:41:44 +0100

    feat(CallToAction): update CallToAction component with new styling and ripple effect color

--- frontend/src/components/layout/CallToAction/CallToAction.tsx
+++ frontend/src/components/layout/CallToAction/CallToAction.tsx
@@ -5,8 +5,8 @@ import Section from '../../ui/Section'
 
 export default function CallToAction() {
   return (
-    <Section className="relative flex sm:h-[400px] h-[450px] w-full items-center justify-center overflow-hidden bg-gray-950 text-center sm:px-0 px-5 ">
-      <Ripple />
+    <Section className="relative flex w-full items-center justify-center overflow-hidden py-32 text-center sm:px-0 px-5 border-y border-white/5 bg-zinc-950/20 backdrop-blur-sm">
+      <Ripple color="rgba(215, 107, 48, 0.05)" />
       <CallToActionContent />
     </Section>
   )
