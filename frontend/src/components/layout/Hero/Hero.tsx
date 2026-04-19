ecc31cc9491f4420ef00a2b312e52dfd0b676338
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2026-04-19 18:34:08 +0100

    feat(Hero): enhance Hero component with improved structure and styling

--- frontend/src/components/layout/Hero/Hero.tsx
+++ frontend/src/components/layout/Hero/Hero.tsx
@@ -2,22 +2,16 @@
 
 import React from "react";
 import Section from "../../ui/Section";
-import { useAOS } from "@/src/hooks/useAOS";
 import BackgroundLayer from "../../ui/BackgroundLayer";
 import HeroContent from "./HeroContent";
-import { useWindowSize } from "@/src/hooks/useWindowSize";
 import { useVanta } from "@/src/hooks/useVanta";
 
 const Hero: React.FC = () => {
-
-    useAOS(1000);
-    useWindowSize();
     useVanta();
-
     return (
         <Section
             id="hero"
-            className="relative w-full min-h-screen flex items-center justify-center overflow-hidden pt-16 md:pt-0"
+            className="relative w-full min-h-screen flex items-center justify-center overflow-hidden pt-16 md:pt-10"
         >
             <BackgroundLayer id="hero-bg" ariaHidden />
             <HeroContent />
