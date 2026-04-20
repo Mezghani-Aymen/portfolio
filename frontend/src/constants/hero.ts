e14b566d492a9f62f98669e33020b2255475279c
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2026-04-20 10:54:01 +0100

    feat(hero): add roles and tech badges constants

---
+++ frontend/src/constants/hero.ts
@@ -0,0 +1,7 @@
+export const roles = [
+    "Full-Stack Developer",
+    "Creative Problem Solver",
+    "Software Engineer",
+];
+
+export const techBadges = ["Next.js", "Nestjs", "SQL/PLSQL", "MongoDB", "Docker"];
